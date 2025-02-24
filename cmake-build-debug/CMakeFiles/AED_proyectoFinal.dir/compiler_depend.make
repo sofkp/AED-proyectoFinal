d49c5108d6966e5d299fefdd95241d43db25d39b
flaqq0 <flavia.honores@utec.edu.pe>
2025-02-24 18:11:53 -0500

    commit

---
+++ cmake-build-debug/CMakeFiles/AED_proyectoFinal.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for AED_proyectoFinal.
+# This may be replaced when dependencies are built.
---
+++ cmake-build-debug/CMakeFiles/c.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/c.dir/prueba3.cpp.obj"
+  "CMakeFiles/c.dir/prueba3.cpp.obj.d"
+  "c.exe"
+  "c.exe.manifest"
+  "c.pdb"
+  "libc.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/c.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
